38f9a817b1d8ff75cea9dea31463dc896aaf0a16
Danny <yanez.daniel0101@gmail.com>
2018-06-01 16:38:48 -0500

    added PersonSayHi

---
+++ PersonSayHello/PersonSayHello/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonSayHello
+{
+    class Program
+    {
+        class Person
+        {
+            int age;
+            string name;
+            public void SayHi()
+            {
+                Console.WriteLine("Hi");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.SayHi();
+        }
+    }
+}
