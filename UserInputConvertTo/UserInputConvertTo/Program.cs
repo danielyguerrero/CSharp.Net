fe2f0a41bf9a8c9542e12cdc0c214b3b13f182d0
Danny <yanez.daniel0101@gmail.com>
2018-06-01 15:42:06 -0500

    initial commit

---
+++ UserInputConvertTo/UserInputConvertTo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInputConvertTo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You are {0} years old", age);
+        }
+    }
+}
