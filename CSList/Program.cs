60c4a3a09752fe97ec383575272c294ffd68523c
Daniel Y. Guerrero <yanez.daniel0101@gmail.com>
2018-10-25 20:51:31 -0500

    added list iteration

--- CSList/Program.cs
+++ CSList/Program.cs
@@ -20,9 +20,19 @@ namespace CSList
         shows.Add("Simpsons");
         shows.Add("How I Met Your Mother");
 
-        Console.WriteLine(shows[0]);
+        // Console.WriteLine(shows[0]);
 
-        Console.WriteLine("There are currently {0} shows in this list.", shows.Count);
+        // Console.WriteLine("There are currently {0} shows in this list.", shows.Count);
+
+
+        //===================    Iterating through the list    =========================
+
+
+        Console.WriteLine("The T.V Shows I have watched are: ");
+        for(var i = 0; i < shows.Count; i++){
+            Console.WriteLine("=> "+ shows[i]);
+        }
+
 
 
 
